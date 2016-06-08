bb03b5f136b941a30de13ef1ac59eaf6f6cc7b53
zhantong <zhantong1994@163.com>
2016-06-08 21:52:41 +0800

    Initial commit

---
+++ app/src/main/java/com/polarxiong/camerademo/CameraPreview.java
@@ -0,0 +1,53 @@
+package com.polarxiong.camerademo;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private static final String TAG = "CameraPreview";
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+    public CameraPreview(Context context) {
+        super(context);
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+    }
+
+    public Camera getCameraInstance() {
+        if (mCamera == null) {
+            try {
+                mCamera = Camera.open();
+            } catch (Exception e) {
+                Log.d(TAG, "camera is not available");
+            }
+        }
+        return mCamera;
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        getCameraInstance();
+        try {
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        mHolder.removeCallback(this);
+        mCamera.setPreviewCallback(null);
+        mCamera.stopPreview();
+        mCamera.release();
+        mCamera = null;
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+    }
+}
---
+++ app/src/main/java/com/polarxiong/camerademo/MainActivity.java
@@ -0,0 +1,40 @@
+package com.polarxiong.camerademo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+/**
+ * Created by zhantong on 16/4/28.
+ */
+public class MainActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final CameraPreview mPreview = new CameraPreview(this);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+
+        SettingsFragment.passCamera(mPreview.getCameraInstance());
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+        SettingsFragment.setDefault(PreferenceManager.getDefaultSharedPreferences(this));
+        SettingsFragment.init(PreferenceManager.getDefaultSharedPreferences(this));
+
+        Button buttonSettings = (Button) findViewById(R.id.button_settings);
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.camera_preview, new SettingsFragment()).addToBackStack(null).commit();
+            }
+        });
+    }
+
+    public void onPause() {
+        finish();
+        super.onPause();
+    }
+}
